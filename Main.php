f0ae616d3efd68513f9c0e2f93b7091734424e12
Rebecabrk <125959502+Rebecabrk@users.noreply.github.com>
2024-03-28 13:01:37 +0200

    initial files

---
+++ Main.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page Title</title>
+    </head>
+    <body>
+
+        <?php
+            include 'components/navbar/navbar.html'
+        ?>
+
+    </body>
+</html>
