ec1ecc15f88ff2c1f8a1282b6f07c03f8755db7b
Rebecabrk <125959502+Rebecabrk@users.noreply.github.com>
2024-03-28 19:33:28 +0200

    home page

--- HomePage.php
+++ HomePage.php
@@ -1,16 +1,61 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Page Title</title>
-        <link href="css/HomePage.css" rel="stylesheet" type="text/css">
-    </head>
-    <body>
 
-        <?php
-            include 'components/navbar.html'
+<head>
+    <title>Page Title</title>
+    <link href="css/HomePage.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <?php
+    include 'components/navbar.html'
         ?>
+    <div id="first_screen" class="full_screen">
+        <h1>
+            Your child's growth
+        </h1>
+        <div>
+            <nav>
+                <ul>
+                    <a href="#second_screen">
+                        <li>
+                            Learn more
+                            <span></span><span></span><span></span><span></span>
+                        </li>
+                    </a>
+                </ul>
+            </nav>
+        </div>
+    </div>
 
-        <button class="learn_more_button">Learn More!</button>
+    <div id="second_screen" class="full_screen">
+        <div class="row">
+            <div class="col">
+                <h1>The manager</h1>
+                <p> 
+                    Empowering Parents.Nurturing Children.Collecting Memories.Building Futures.  
+                </p>
+            </div>
+            <div class="col">
+                <div class="card card1">
+                    <h5>Empowering Parents</h5>
+                    <p>Helping parents keep track of food and sleep schedules of their children through just a click.</p>   
+                </div>
+                <div class="card card2">
+                    <h5>Nurturing Children</h5>
+                    <p>The children will benefit from the parents' better organization, because this way parents will have more time for family time.</p>      
+                </div>
+                <div class="card card3">
+                    <h5>Collecting Memories</h5> 
+                    <p>A great way to store and visualize all beautiful memories made throughout the children's development process.</p>     
+                </div>
+                <div class="card card4">
+                    <h5>Building Futures</h5>   
+                    <p>Creating an organized way of keeping track of all the information about the children, such that ...</p>   
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
 
-    </body>
 </html>
