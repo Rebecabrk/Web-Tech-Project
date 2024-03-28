feeb00c00e7d74eb0265d1f2aa9a37175d80e985
Rebecabrk <125959502+Rebecabrk@users.noreply.github.com>
2024-03-28 13:45:29 +0200

    initial files

---
+++ HomePage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page Title</title>
+        <link href="css/HomePage.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+
+        <?php
+            include 'components/navbar.html'
+        ?>
+
+        <button class="learn_more_button">Learn More!</button>
+
+    </body>
+</html>
