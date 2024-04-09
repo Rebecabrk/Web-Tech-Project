78d7e803362f704deda3a15e779abe66ac292653
Olaru Marius-Alexandru <marius.alexandru.olaru@gmail.com>
2024-04-09 14:54:38 +0300

    update: new regostration form

--- CreateAccount.php
+++ CreateAccount.php
@@ -1,39 +1,45 @@
 <!DOCTYPE html>
 <html>
-  <head>
-      <title>Create Account Page</title>
-      <link href="css/CreateAccount.css" rel="stylesheet">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  </head>
 
-  <body>
-      <div class="background">
-          <div id="form" class="form_box">
-              <form action="process.php" method="get">
-                  <ul>
-                    <li>
-                      <input type="text" id="first_name" name="user_first_name" placeholder="First Name" required/>
-                    </li>
-                    <li>
-                      <input type="text" id="last_name" name="user_last_name" placeholder="Last Name" required/>
-                    </li>
-                    <li>
-                      <input type="email" id="mail" name="user_mail" placeholder="Email" required/>
-                    </li>
-                    <li>
-                      <input type="password" id="password" name="user_password" placeholder="Password" required/>
-                    </li>
-                    <hr>
-                    <li class="button">
-                        <button type="submit">Create Account</button>
-                    </li>
-                  </ul>
-                </form>
-          </div>
+<head>
+  <title>Create Account Page</title>
+  <link href="css/CreateAccount.css" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
 
-          <div id="text">
-              One step closer to building your child's bright future!
-          </div>
-      </div>
-  </body>
+<body>
+  <div class="background">
+    <div class="wrapper">
+      <form action="process.php" method="get">
+        <h1>Create Account</h1>
+        <hr>
+        <div class="input-box">
+          <i class='bx bx-user-circle'></i>
+          <input type="text" id="first_name" name="user_first_name" placeholder="First Name" required />
+        </div>
+        <div class="input-box">
+          <i class='bx bx-user-circle'></i>
+          <input type="text" id="last_name" name="user_last_name" placeholder="Last Name" required />
+        </div>
+        <div class="input-box">
+          <i class='bx bx-envelope'></i>
+          <input type="email" id="mail" name="user_mail" placeholder="Email" required />
+        </div>
+        <div class="input-box">
+          <i class='bx bx-lock-alt'></i>
+          <input type="password" id="password" name="user_password" placeholder="Password" required />
+        </div>
+        <hr>
+        <div class="button">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+    <div id="text">
+      One step closer to building your child's bright future!
+    </div>
+  </div>
+</body>
+
 </html>
--- HomePage.php
+++ HomePage.php
@@ -1,93 +1,101 @@
 <!DOCTYPE html>
 
 <html>
-    <head>
-        <title>Landing Page</title>
-        <link href="css/HomePage.css" rel="stylesheet">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
 
-    <body>
-        <?php include 'components/navbar.html' ?>
-        <div id="first_screen" class="full_screen">
-            <div class="wrapper">
-                <h1 class="tracking-in-expand">
-                    Your child's growth
-                </h1>
-                <div class="buttons-container">
-                    <nav>
-                        <ul>
-                            <a href="#second_screen" class="lm_btn">
-                                <li>
-                                    Learn more
-                                    <span></span><span></span><span></span><span></span>
-                                </li>
-                            </a>
-                        </ul>
-                    </nav>
-                    <nav>
-                        <ul>
-                            <a href="CreateAccount.php" class="ca_btn">
-                                <li>
-                                    Create account
-                                    <span></span><span></span><span></span><span></span>
-                                </li>
-                            </a>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
+<head>
+  <title>Landing Page</title>
+  <link href="css/HomePage.css" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+  <?php include 'components/navbar.html' ?>
+  <div id="first_screen" class="full_screen">
+    <div class="wrapper">
+      <h1 class="tracking-in-expand">
+        Your child's growth
+      </h1>
+      <div class="buttons-container">
+        <nav>
+          <ul>
+            <a href="#second_screen" class="lm_btn">
+              <li>
+                Learn more
+                <span></span><span></span><span></span><span></span>
+              </li>
+            </a>
+          </ul>
+        </nav>
+        <nav>
+          <ul>
+            <a href="CreateAccount.php" class="ca_btn">
+              <li>
+                Create account
+                <span></span><span></span><span></span><span></span>
+              </li>
+            </a>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </div>
+
+  <div id="second_screen" class="full_screen">
+    <div class="row">
+      <div class="title-container">
+        <h1>The manager</h1>
+        <p>
+          Empowering Parents. Nurturing Children. </br> Collecting Memories. Building Futures.
+        </p>
+      </div>
+      <div class="card-container">
+        <div class="card card1">
+          <h3>Empowering Parents</h3>
+          <p>Helping parents keep track of food and sleep schedules of their children through just a click.</p>
         </div>
-            
-        <div id="second_screen" class="full_screen">
-            <div class="row">
-                <div class="title-container">
-                    <h1>The manager</h1>
-                    <p>
-                        Empowering Parents. Nurturing Children. </br> Collecting Memories. Building Futures.  
-                    </p>
-                </div>
-                <div class="card-container">
-                    <div class="card card1">
-                        <h3>Empowering Parents</h3>
-                        <p>Helping parents keep track of food and sleep schedules of their children through just a click.</p>   
-                    </div>
-                    <div class="card card2">
-                        <h3>Nurturing Children</h3>
-                        <p>The children will benefit from the parents' better organization, because this way parents will have more time for family time.</p>      
-                    </div>
-                    <div class="card card3">
-                        <h3>Collecting Memories</h3> 
-                        <p>A great way to store and visualize all beautiful memories made throughout the children's development process.</p>     
-                    </div>
-                    <div class="card card4">
-                        <h3>Building Futures</h3>   
-                        <p>Creating an organized way of keeping track of all the information about the children, such that ...</p>   
-                    </div>
-                </div>
-            </div>
+        <div class="card card2">
+          <h3>Nurturing Children</h3>
+          <p>The children will benefit from the parents' better organization, because this way parents will have more
+            time for family time.</p>
+        </div>
+        <div class="card card3">
+          <h3>Collecting Memories</h3>
+          <p>A great way to store and visualize all beautiful memories made throughout the children's development
+            process.</p>
+        </div>
+        <div class="card card4">
+          <h3>Building Futures</h3>
+          <p>Creating an organized way of keeping track of all the information about the children, such that ...</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="third_screen" class="full_screen">
+    <div class="third_screen_container">
+      <div class="center_element">
+        <div class="wrapper2">
+          <p class="content_paragraph">Early childhood, from birth to age 5, is a critical period for brain development.
+            Stimulating environments and nurturing interactions during this time can have a significant impact on a
+            child's cognitive, social, and emotional development.</p>
+          <div class="card card1"></div>
         </div>
 
-        <div id="third_screen" class="full_screen">
-            <div class="third_screen_container">
-                <div class="center_element">
-                    <div class="wrapper2">
-                        <p class="content_paragraph">Early childhood, from birth to age 5, is a critical period for brain development. Stimulating environments and nurturing interactions during this time can have a significant impact on a child's cognitive, social, and emotional development.</p>
-                        <div class="card card1"></div>
-                    </div>
-
-                    <div class="wrapper2">
-                        <div class="card card2"></div>
-                        <p class="content_paragraph">Play is essential for children's learning and development. It helps them develop important skills such as problem-solving, creativity, communication, and socialization. Through play, children explore the world around them and make sense of their experiences.</p>
-                    </div>
-                </div>
-            </div>
+        <div class="wrapper2">
+          <div class="card card2"></div>
+          <p class="content_paragraph">Play is essential for children's learning and development. It helps them develop
+            important skills such as problem-solving, creativity, communication, and socialization. Through play,
+            children explore the world around them and make sense of their experiences.</p>
         </div>
-    </body>
+      </div>
+    </div>
+  </div>
+</body>
+
 </html>
 
-    <!--navbar -->
-    <!-- <div class="container">
+<!--navbar -->
+<!-- <div class="container">
         <a href="#" class="toggleBox">
             <span class="icon"></span>
         </a>
@@ -128,4 +136,4 @@
         });
     </script> -->
 
-    <!-- <button class="learn_more_button">Learn More!</button> -->
+<!-- <button class="learn_more_button">Learn More!</button> -->
