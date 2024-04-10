203e8cdd229f5153a3b03027992520526e7d2a8f
Rebecabrk <125959502+Rebecabrk@users.noreply.github.com>
2024-04-10 15:17:10 +0300

    profile/dashboard

--- CreateAccount.php
+++ CreateAccount.php
@@ -2,6 +2,7 @@
 <html>
 
 <head>
+  <meta charset="utf-8">
   <title>Create Account Page</title>
   <link href="css/CreateAccount.css" rel="stylesheet">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
