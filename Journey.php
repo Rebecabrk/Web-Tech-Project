aba54c09f41fe9d560a2e70c0a22e539cb011db8
Olaru Marius-Alexandru <marius.alexandru.olaru@gmail.com>
2024-04-13 20:01:55 +0300

    repared the nav bar from journey

--- Journey.php
+++ Journey.php
@@ -6,6 +6,7 @@
     <meta charset="utf-8">
     <title>The Journey Page</title>
     <link href="css/Journey.css" rel="stylesheet">
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
