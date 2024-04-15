e3f773ec518d1e83cbf6cf87b5793443af9c8144
Olaru Marius-Alexandru <marius.alexandru.olaru@gmail.com>
2024-04-15 18:51:51 +0300

    lil update

--- Profile.php
+++ Profile.php
@@ -53,19 +53,16 @@
                     <hr>
                     <div class="input-box">
                         <i class='bx bx-user-circle'></i>
-                        <input type="text" id="first_name" name="user_first_name" placeholder="First Name" required />
-                    </div>
-                    <div class="input-box">
-                        <i class='bx bx-user-circle'></i>
                         <input type="text" id="last_name" name="user_last_name" placeholder="Last Name" required />
                     </div>
                     <div class="input-box">
-                        <i class='bx bx-envelope'></i>
-                        <input type="email" id="mail" name="user_mail" placeholder="Email" required />
+                        <i class='bx bx-timer'></i>
+                        <input type="text" id="schedules" name="user_schedules" placeholder="Schedules" required />
                     </div>
                     <div class="input-box">
-                        <i class='bx bx-lock-alt'></i>
-                        <input type="password" id="password" name="user_password" placeholder="Password" required />
+                        <i class='bx bx-plus-medical'></i>
+                        <input type="text" id="medical-record" name="user_mr" placeholder="New Medical Record"
+                            required />
                     </div>
                     <hr>
                     <div class="button">
@@ -80,79 +77,77 @@
                 Do you have another child?
             </div>
         </div>
-    </div>
 
-
-    <div class="sidebar" id="sidebar">
-        <div class="logo-details">
-            <i class="bx bx-menu" id="btn"></i>
+        <div class="sidebar" id="sidebar">
+            <div class="logo-details">
+                <i class="bx bx-menu" id="btn"></i>
+            </div>
+            <ul class="nav-list">
+                <li>
+                    <i class="bx bx-search"></i>
+                    <input type="text" placeholder="Search..." />
+                    <span class="tooltip">Search</span>
+                </li>
+                <li>
+                    <a href="HomePage.php">
+                        <i class='bx bx-home'></i>
+                        <span class="links_name">Home</span>
+                    </a>
+                    <span class="tooltip">Home</span>
+                </li>
+                <li>
+                    <a href="Dashboard.php">
+                        <i class="bx bx-grid-alt"></i>
+                        <span class="links_name">Dashboard</span>
+                    </a>
+                    <span class="tooltip">Dashboard</span>
+                </li>
+                <li>
+                    <a href="Profile.php">
+                        <i class="bx bx-user"></i>
+                        <span class="links_name">Profile</span>
+                    </a>
+                    <span class="tooltip">Profile</span>
+                </li>
+                <li>
+                    <a href="Journal.php">
+                        <i class='bx bx-book-bookmark'></i>
+                        <span class="links_name">Journal</span>
+                    </a>
+                    <span class="tooltip">Journal</span>
+                </li>
+                <li>
+                    <a href="Journey.php">
+                        <i class="bx bx-heart"></i>
+                        <span class="links_name">The Journey</span>
+                    </a>
+                    <span class="tooltip">The Journey</span>
+                </li>
+                <li>
+                    <a href="Settings.php">
+                        <i class="bx bx-cog"></i>
+                        <span class="links_name">Settings</span>
+                    </a>
+                    <span class="tooltip">Settings</span>
+                </li>
+                <li>
+                    <a href="About.php">
+                        <i class='bx bx-smile'></i>
+                        <span class="links_name">About Us</span>
+                    </a>
+                    <span class="tooltip">About Us</span>
+                </li>
+                <li>
+                    <a href="Documentation.php">
+                        <i class='bx bxs-file-doc'></i>
+                        <span class="links_name">Documentation</span>
+                    </a>
+                    <span class="tooltip">Documentation</span>
+                </li>
+            </ul>
         </div>
-        <ul class="nav-list">
-            <li>
-                <i class="bx bx-search"></i>
-                <input type="text" placeholder="Search..." />
-                <span class="tooltip">Search</span>
-            </li>
-            <li>
-                <a href="HomePage.php">
-                    <i class='bx bx-home'></i>
-                    <span class="links_name">Home</span>
-                </a>
-                <span class="tooltip">Home</span>
-            </li>
-            <li>
-                <a href="Dashboard.php">
-                    <i class="bx bx-grid-alt"></i>
-                    <span class="links_name">Dashboard</span>
-                </a>
-                <span class="tooltip">Dashboard</span>
-            </li>
-            <li>
-                <a href="Profile.php">
-                    <i class="bx bx-user"></i>
-                    <span class="links_name">Profile</span>
-                </a>
-                <span class="tooltip">Profile</span>
-            </li>
-            <li>
-                <a href="Journal.php">
-                    <i class='bx bx-book-bookmark'></i>
-                    <span class="links_name">Journal</span>
-                </a>
-                <span class="tooltip">Journal</span>
-            </li>
-            <li>
-                <a href="Journey.php">
-                    <i class="bx bx-heart"></i>
-                    <span class="links_name">The Journey</span>
-                </a>
-                <span class="tooltip">The Journey</span>
-            </li>
-            <li>
-                <a href="Settings.php">
-                    <i class="bx bx-cog"></i>
-                    <span class="links_name">Settings</span>
-                </a>
-                <span class="tooltip">Settings</span>
-            </li>
-            <li>
-                <a href="About.php">
-                    <i class='bx bx-smile'></i>
-                    <span class="links_name">About Us</span>
-                </a>
-                <span class="tooltip">About Us</span>
-            </li>
-            <li>
-                <a href="Documentation.php">
-                    <i class='bx bxs-file-doc'></i>
-                    <span class="links_name">Documentation</span>
-                </a>
-                <span class="tooltip">Documentation</span>
-            </li>
-        </ul>
-    </div>
 
-    <script src="components\SideNavBar\SideNavBar.js"></script>
+        <script src="components\SideNavBar\SideNavBar.js"></script>
 </body>
 
 </html>
