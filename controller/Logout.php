65e481b6da5182ee9c91556b9c9701e951551ced
Rebecabrk <125959502+Rebecabrk@users.noreply.github.com>
2024-04-25 15:25:10 +0300

    moved view/Logout.php to controller/Logout.php

--- controller/Logout.php
+++ controller/Logout.php
@@ -1,10 +1,10 @@
 <?php
 
-require __DIR__ . "/user_acces/connected-user.php"; // here we are verifying if the user is connected
+require ("../model/user_acces/connected-user.php"); // here we are verifying if the user is connected
 
 session_start();
 
-$mysqli = require __DIR__ . "/user_acces/database.php";
+$mysqli = require ("../model/user_acces/database.php");
 $sql = "UPDATE user SET session_id = NULL WHERE session_id = '" . session_id() . "'";
 $result = $mysqli->query($sql);
 
