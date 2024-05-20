1f3cb47ea630c317425c5452779820e7229d9e3c
Rebecabrk <125959502+Rebecabrk@users.noreply.github.com>
2024-05-20 15:14:38 +0300

    Journal memories db works!

--- model/TextEditor_model.php
+++ model/TextEditor_model.php
@@ -5,7 +5,7 @@ $title = $_POST['title'];
 $text = $_POST['text'];
 $data = date('Y-m-d H:i:s');
 $user_id = 2;
-$sql = "INSERT INTO memories (user_id, data_creare, title, usr_text) VALUES (?,?,?,?)";
+$sql = "INSERT INTO memories (user_id, creation_date, title, text) VALUES (?,?,?,?)";
 $stmt = $mysqli->prepare($sql);
 $stmt->bind_param("ssss", $user_id, $data, $title, $text);
 try {
