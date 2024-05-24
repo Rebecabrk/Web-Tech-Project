1f3cbc0947543d06b7290ba601705d12055ec846
Rebecabrk <125959502+Rebecabrk@users.noreply.github.com>
2024-05-24 13:42:03 +0300

    fixed Journal again..........

--- controller/TextEditor.php
+++ controller/TextEditor.php
@@ -4,8 +4,8 @@ if(!isset($_COOKIE['In_God_We_Trust']))
     header("Location: LandingPage.php");
 
 require ("../model/TextEditor_model.php");
-$backgroundStyle = getBackground();
+$backgroundInformation = getBackground();
 
-$isMemorySet = isset($_POST['memoryId']) ? $_POST['memoryId'] : null;
+$isMemorySet = isset($_GET['memoryId']) ? $_GET['memoryId'] : null;
 
 include ("../view/TextEditor_view.php");
