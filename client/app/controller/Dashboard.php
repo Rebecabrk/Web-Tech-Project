0312faf14343394f1eb64596d63c6f9d9098e79f
Olaru Marius-Alexandru <marius.alexandru.olaru@gmail.com>
2024-06-23 15:26:20 +0300

    start doin the medical record services

--- client/app/controller/Dashboard.php
+++ client/app/controller/Dashboard.php
@@ -3,6 +3,10 @@
 if(!isset($_COOKIE['In_God_We_Trust']) || $_COOKIE['Child_Picker'] === "nothing")
     header("Location: LandingPage.php");
 
+require ("../model/Dashboard_model.php");
+
+$MRecordXML = getMRecordXML($_COOKIE['Child_Picker']);
+
 include ("../view/Dashboard_view.php");
 
 include ("../view/components/SideNavBar/SideNavBar.php");
---
+++ client/app/model/Dashboard_model.php
@@ -0,0 +1,20 @@
+<?php
+
+function getMRecordXML($user_id)
+{
+    $url = 'http://localhost/Web-Tech-Project/services/medical_record/' . $user_id;
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+
+    if ($response === false) {
+        $error_msg = curl_error($ch);
+        curl_close($ch);
+        die('Error occurred while getting data: ' . $error_msg);
+    }
+
+    curl_close($ch);
+
+    return $response;
+}
